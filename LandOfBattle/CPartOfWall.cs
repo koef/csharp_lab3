d15594176fb0fe80408b348d641adf2fa5341091
koef <rudnev.vv@gmail.com>
2015-11-29 13:44:50 +0200

    Creating class for a part of wall.

---
+++ LandOfBattle/CPartOfWall.cs
@@ -0,0 +1,62 @@
+﻿using LandOfBattle.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandOfBattle
+{
+    class CPartOfWall : CImageBase
+    {
+        public int partType = 0;
+        public const int Brick = 0;
+        public const int Target = 1;
+        public const int Empty = 2;
+
+        public int TypeOfPart
+        {
+            get { return partType; }
+        }
+
+        public CPartOfWall() : base(Resources.brick)
+        {
+            partType = Brick;
+        }
+
+
+        public CPartOfWall(int _partType) : base(new Bitmap(35, 35))
+        {
+            ChangeState(_partType);
+        }
+
+        private void ChangeState(int _partType)
+        {
+            switch (_partType)
+            {
+                case 0:
+                    _bitmap = Resources.brick;
+                    partType = Brick;
+                    break;
+                case 1:
+                    _bitmap = Resources.target;
+                    partType = Target;
+                    break;
+                case 2:
+                    _bitmap = new Bitmap(35, 35);
+                    partType = Empty;
+                    break;
+                default:
+                    _bitmap = Resources.brick;
+                    partType = Brick;
+                    break;
+            }
+        }
+
+        public void Destroy()
+        {
+            ChangeState(Empty);
+        }
+    }
+}
--- LandOfBattle/Properties/Resources.Designer.cs
+++ LandOfBattle/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace LandOfBattle.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap brick {
+            get {
+                object obj = ResourceManager.GetObject("brick", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cannon {
             get {
                 object obj = ResourceManager.GetObject("cannon", resourceCulture);
@@ -96,6 +106,16 @@ namespace LandOfBattle.Properties {
         internal static System.IO.UnmanagedMemoryStream CannonSound {
             get {
                 return ResourceManager.GetStream("CannonSound", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap target {
+            get {
+                object obj = ResourceManager.GetObject("target", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
